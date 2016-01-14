e60e09ce77a8f369c3b233ec6bc80d7431801a6e
Jonas Haag <jonas@lophus.org>
2016-01-14 14:53:21 +0100

    Refactoring; add 'send_mass_mail' convenience function

--- mail_templated_simple/__init__.py
+++ mail_templated_simple/__init__.py
@@ -1,11 +1,21 @@
 import django
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, get_connection
 from django.template import Context
 from django.template.loader_tags import BlockNode
 from django.template.loader import get_template
 
 
-def send_mail(template_name, context, from_email, recipient_list, fail_silently=False):
+def email_message(template_name, context, *message_args, **message_kwargs):
+    """Construct an email message from a template. The template is passed
+    `context` as template context.
+
+    The template is expected to contain ``{% block subject %}`` and
+    ``{% block body %}``.  In addition, an HTML-formatted body may be added by
+    the use of a ``{% block html %}``.  Both subject and body default to `None`
+    if absent.
+
+    :rtype: ``EmailMultiAlternatives``
+    """
     template = get_template(template_name)
 
     if django.VERSION >= (1, 8):
@@ -33,7 +43,42 @@ def send_mail(template_name, context, from_email, recipient_list, fail_silently=
     if html:
         html = html.render(context).strip('\r\n')
 
-    msg = EmailMultiAlternatives(subject, body, from_email, recipient_list)
+    mail = EmailMultiAlternatives(subject, body, *message_args, **message_kwargs)
     if html:
-        msg.attach_alternative(html, 'text/html')
-    msg.send(fail_silently=fail_silently)
+        mail.attach_alternative(html, 'text/html')
+    return mail
+
+
+def send_mail(template_name, context, from_email, recipient_list,
+              fail_silently=False, auth_user=None, auth_password=None,
+              connection=None, html_message=None):
+    """This is what :func:`django.core.mail.send_mail` is for normal messages.
+
+    Note that instead of the `subject` and `message` parameters this convience
+    function expects a `template_name` and `context` from which subject and
+    message are derived.
+    """
+    connection = connection or get_connection(username=auth_user,
+                                    password=auth_password,
+                                    fail_silently=fail_silently)
+    mail = email_message(template_name, context, from_email, recipient_list,
+                         connection=connection)
+    return mail.send()
+
+
+def send_mass_mail(template_name, datatuple, fail_silently=False, auth_user=None,
+                   auth_password=None, connection=None):
+    """This is what :func:`django.core.mail.send_mass_mail` is for normal messages.
+
+    Note that the layout of `datatuple` is different from Django's.
+    It is expected to be in this format::
+
+        (context, from_email, recipient_list)
+    """
+    connection = connection or get_connection(username=auth_user,
+                                    password=auth_password,
+                                    fail_silently=fail_silently)
+    messages = [email_message(template_name, context, sender, recipient,
+                             connection=connection)
+                for context, sender, recipient in datatuple]
+    return connection.send_messages(messages)
--- mail_templated_simple_tests/tests.py
+++ mail_templated_simple_tests/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core import mail
-from mail_templated_simple import send_mail
+import mail_templated_simple
 
 
 class Tests(TestCase):
@@ -8,7 +8,10 @@ class Tests(TestCase):
         ctx = {'foo': "Foo"}
         from_email = 'from@example.com'
         recipient_list = ['to@example.com', 'to2@example2.com']
-        send_mail("mail_templated_simple_tests/full_example.tpl", ctx, from_email, recipient_list)
+        mail_templated_simple.send_mail(
+            "mail_templated_simple_tests/full_example.tpl",
+            ctx, from_email, recipient_list,
+        )
 
         self.assertEqual(len(mail.outbox), 1)
         msg = mail.outbox[0]
@@ -20,7 +23,39 @@ class Tests(TestCase):
         self.assertEqual(msg.alternatives[0], ("Example html, Foo", "text/html"))
 
     def test_without_html(self):
-        send_mail("mail_templated_simple_tests/plaintext_only_example.tpl", None, None, ['to@example.org'])
+        mail_templated_simple.send_mail(
+            "mail_templated_simple_tests/plaintext_only_example.tpl",
+            None, None, ["to@example.com"],
+        )
         self.assertEqual(len(mail.outbox), 1)
-        self.assertFalse(mail.outbox[0].alternatives)
+        self.assertEqual(mail.outbox[0].alternatives, [])
         self.assertEqual(mail.outbox[0].body, "Only plaintext body")
+
+    def test_email_message(self):
+        msg = mail_templated_simple.email_message(
+            "mail_templated_simple_tests/plaintext_only_example.tpl",
+            {}, cc=["cc@cc.cc"],
+        )
+        self.assertEqual(msg.cc, ["cc@cc.cc"])
+
+    def test_send_mass_mail(self):
+        recipient_list1 = ["to@example.com", "to2@example.com"]
+        recipient_list2 = ["to3@example.com"]
+        from_email1 = "from1@example.com"
+        from_email2 = "from2@example.com"
+
+        mail_templated_simple.send_mass_mail(
+            "mail_templated_simple_tests/full_example.tpl",
+            [({'foo': "Test 1"}, from_email1, recipient_list1),
+             ({'foo': "Test 2"}, from_email2, recipient_list2)],
+        )
+
+        self.assertEqual(len(mail.outbox), 2)
+        msg1 = mail.outbox[0]
+        self.assertEqual(msg1.to, recipient_list1)
+        self.assertEqual(msg1.from_email, from_email1)
+        self.assertEqual(msg1.subject, "Example subject, Test 1")
+        msg2 = mail.outbox[1]
+        self.assertEqual(msg2.to, recipient_list2)
+        self.assertEqual(msg2.from_email, from_email2)
+        self.assertEqual(msg2.subject, "Example subject, Test 2")
